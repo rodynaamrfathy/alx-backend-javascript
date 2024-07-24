91a056f56da65aaa0c8496f0879f011233538d6d
rodynaamrfathy <rodynaamr@icloud.com>
2024-07-24 21:16:04 +0000

    ‘UPDATE’

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Use `let` for block-scoped variable
-  let task2 = true; // Use `let` for block-scoped variable
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;    // This will now modify the `task` variable defined outside the block
-    task2 = false;  // This will now modify the `task2` variable defined outside the block
+    const task = true;
+    const task2 = false; 
   }
 
   return [task, task2];
