29551edcfaf743b57bc309a16742a0d6b98c9204
rodynaamrfathy <rodynaamr@icloud.com>
2024-07-30 11:14:42 +0000

    ‘UPDATE’

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+	let map = new Map;
+	
+	map
+		.set("Apples", 10)
+		.set("Tomatoes", 10)
+		.set("Pasta", 1)
+		.set("Rice", 1)
+		.set("Banana", 5);
+
+	return map;
+}
