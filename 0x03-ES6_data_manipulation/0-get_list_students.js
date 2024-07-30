f0cd64e146955c3bd9f44c397c23855f29bd11c3
rodynaamrfathy <rodynaamr@icloud.com>
2024-07-30 19:07:42 +0000

    ‘UPDATE’

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,26 +1,7 @@
-/**
- * Retrieves a list of students.
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
 export default function getListStudents() {
-    const obj1 = {
-        id: 1,
-        firstName: 'Guillaume',
-        location: 'San Francisco',
-    };
-
-    const obj2 = {
-        id: 2,
-        firstName: 'James',
-        location: 'Columbia',
-    };
-
-    const obj3 = {
-        id: 5,
-        firstName: 'Serena',
-        location: 'San Francisco',
-    };
-
-    return [obj1, obj2, obj3];
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
-
